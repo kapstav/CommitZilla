210ffd9b0284ea3149d10ac7a37714fe17b49e01
ekta.srivastava <ektasri@yahoo.com>
2024-03-17 00:02:04 -0700

    Commit 14

--- aone.js
+++ aone.js
@@ -1,5 +1,16 @@
-// Export the function
-module.exports = function helloWorld() {
-    console.log('Hello, World!');
-  }
+// Exporting both functions together
+module.exports = {
+    helloWorld: function() {
+      console.log('Hello, World!');
+    },
+    helloSum: function() {
+      // Define variables
+      var x = 3;
+      var z = 8;
+      // Calculate y
+      var y = x + z;
+      console.log('The sum of x and z is:', y);
+      return y; // Return y for testing purposes
+    }
+  };
   
